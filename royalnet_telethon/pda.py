fb3153a6948af6c43f8e343eee2eb26205bf579e
Stefano Pigozzi <me@steffo.eu>
2021-04-14 04:12:54 +0200

    💥 Massive progress here!

--- royalnet_telethon/pda.py
+++ royalnet_telethon/pda.py
@@ -6,7 +6,6 @@ from __future__ import annotations
 import royalnet.royaltyping as t
 
 import logging
-import asyncio
 import royalnet.engineer as engi
 import telethon as tt
 import telethon.tl.custom as tlc
@@ -18,34 +17,35 @@ log = logging.getLogger(__name__)
 
 
 class TelethonPDAMode(enum.Enum):
+    """
+    .. todo:: Document this.
+    """
+
     GLOBAL = enum.auto()
     CHAT = enum.auto()
     USER = enum.auto()
     CHAT_USER = enum.auto()
 
 
-class TelethonPDA:
+class TelethonPDAImplementation(engi.ConversationListImplementation):
     """
-    A PDA which handles :mod:`royalnet` input and output using a Telegram bot as a source.
+    .. todo:: Document this.
     """
 
-    def __init__(self,
-                 tg_api_id: int,
-                 tg_api_hash: str,
-                 bot_username: str,
-                 mode: TelethonPDAMode = TelethonPDAMode.CHAT_USER,
-                 ):
-        """
-        Create a new :class:`.TelethonPDA` .
+    def _partialcommand_pattern(self, partial) -> str:
+        if partial.syntax:
+            return rf"^/{{name}}(?:@{self.bot_username})?\s+{{syntax}}$"
+        else:
+            return rf"^/{{name}}(?:@{self.bot_username})?$"
 
-        Get API properties `here <https://my.telegram.org/apps>`_.
+    @property
+    def namespace(self):
+        return "telethon"
 
-        :param tg_api_id: The Telegram ``api_id``.
-        :param tg_api_hash: The Telegram ``api_hash``.
-        :param mode: The mode to use for mapping dispensers.
-        """
+    def __init__(self, name: str, tg_api_id: int, tg_api_hash: str, bot_username: str, bot_token: str,
+                 mode: TelethonPDAMode = TelethonPDAMode.CHAT_USER, extensions = None):
 
-        log.debug(f"Creating new TelethonPDA...")
+        super().__init__(name=name, extensions=extensions)
 
         self.dispensers: dict[t.Any, engi.Dispenser] = {}
         """
@@ -58,24 +58,43 @@ class TelethonPDA:
         :class:`~royalnet.engineer.dispenser.Dispenser`.
         """
 
-        self.client: tt.TelegramClient = tt.TelegramClient("bot", api_id=tg_api_id, api_hash=tg_api_hash)
-        """
-        The :mod:`telethon` Telegram _client that this PDA will use to interface with Telegram.
-        """
-
-        self._register_events()
-
         self.mode: TelethonPDAMode = mode
         """
         The mode to use for mapping dispensers.
         """
 
-        self.bot_username: str = bot_username
+        self.tg_api_id: int = tg_api_id
+        """
+        .. todo:: Document this.
+        """
 
-    def _register_events(self):
-        self.client.add_event_handler(callback=self._message_new, event=tt.events.NewMessage())
-        self.client.add_event_handler(callback=self._message_edit, event=tt.events.MessageEdited())
-        self.client.add_event_handler(callback=self._message_delete, event=tt.events.MessageDeleted())
+        self.tg_api_hash: str = tg_api_hash
+        """
+        .. todo:: Document this.
+        """
+
+        self.bot_username: str = bot_username
+        """
+        .. todo:: Document this.
+        """
+
+        self.bot_token: str = bot_token
+        """
+        .. todo:: Document this.
+        """
+
+    def _register_events(self, client):
+        """
+        .. todo:: Document this.
+        """
+
+        self.log.info("Registering Telethon events...")
+        self.log.debug("Registering NewMessage event...")
+        client.add_event_handler(callback=self._message_new, event=tt.events.NewMessage())
+        self.log.debug("Registering MessageEdited event...")
+        client.add_event_handler(callback=self._message_edit, event=tt.events.MessageEdited())
+        self.log.debug("Registering MessageDeleted event...")
+        client.add_event_handler(callback=self._message_delete, event=tt.events.MessageDeleted())
         # self._client.add_event_handler(callback=self._message_read, _event=tt.events.MessageRead())
         # self._client.add_event_handler(callback=self._chat_action, _event=tt.events.ChatAction())
         # self._client.add_event_handler(callback=self._user_update, _event=tt.events.UserUpdate())
@@ -84,6 +103,10 @@ class TelethonPDA:
         # self._client.add_event_handler(callback=self._album, _event=tt.events.Album())
 
     def _determine_key(self, event: tlc.message.Message):
+        """
+        .. todo:: Document this.
+        """
+
         if self.mode == TelethonPDAMode.GLOBAL:
             return None
         elif self.mode == TelethonPDAMode.USER:
@@ -102,101 +125,50 @@ class TelethonPDA:
             raise TypeError("Invalid mode")
 
     async def _message_new(self, event: tlc.message.Message):
+        """
+        .. todo:: Document this.
+        """
+
         await self.put_projectile(
             key=self._determine_key(event),
-            proj=TelegramMessageReceived(event=event),
+            projectile=TelegramMessageReceived(event=event),
         )
 
     async def _message_edit(self, event: tlc.message.Message):
+        """
+        .. todo:: Document this.
+        """
+
         await self.put_projectile(
             key=self._determine_key(event),
-            proj=TelegramMessageEdited(event=event),
+            projectile=TelegramMessageEdited(event=event),
         )
 
     async def _message_delete(self, event: tlc.message.Message):
+        """
+        .. todo:: Document this.
+        """
+
         await self.put_projectile(
             key=self._determine_key(event),
-            proj=TelegramMessageDeleted(event=event),
+            projectile=TelegramMessageDeleted(event=event),
         )
 
-    async def run(self, bot_token: str) -> t.NoReturn:
-        """
-        Run the main loop of the :class:`.ConsolePDA` for ``cycles`` cycles, or unlimited cycles if the parameter is
-        :data:`True`.
-        """
-        # Login to the Telegram API
-        self.client: tt.TelegramClient = await self.client.start(bot_token=bot_token)
-        await self.client.connect()
-        await self.client.get_me()
-        await self.client.catch_up()
-        await self.client.run_until_disconnected()
+    async def run(self) -> t.NoReturn:
+        client = tt.TelegramClient(
+            "bot",
+            api_id=self.tg_api_id,
+            api_hash=self.tg_api_hash
+        )
 
-    def register_conversation(self, conv: engi.Conversation) -> None:
-        """
-        Register a new conversation in the PDA.
-
-        :param conv: The conversation to register.
-        """
-        log.info(f"Registering conversation: {conv!r}")
-        self.conversations.append(conv)
-
-    def unregister_conversation(self, conv: engi.Conversation) -> None:
-        """
-        Unregister a conversation from the PDA.
-
-        :param conv: The conversation to unregister.
-        """
-        log.info(f"Unregistering conversation: {conv!r}")
-        self.conversations.remove(conv)
-
-    def register_partial(self, part: engi.PartialCommand, names: t.List[str]) -> engi.Command:
-        """
-        Register a new :class:`~royalnet.engineer.command.PartialCommand` in the PDA, converting it to a
-        :class:`royalnet.engineer.Command` in the process.
-
-        :param part: The :class:`~royalnet.engineer.command.PartialCommand` to register.
-        :param names: The :attr:`~royalnet.engineer.command.Command.names` to register the command with.
-        :return: The resulting :class:`~royalnet.engineer.command.Command`.
-        """
-        log.debug(f"Completing partial: {part!r}")
-        if part.syntax:
-            command = part.complete(pattern=rf"^/{{name}}(?:@{self.bot_username})?\s+{{syntax}}$", names=names)
-        else:
-            command = part.complete(pattern=rf"^/{{name}}(?:@{self.bot_username})?$", names=names)
-        self.register_conversation(command)
-        return command
-
-    async def put_projectile(self, key: t.Any, proj: engi.Projectile) -> None:
-        """
-        Insert a new projectile into the dispenser.
-
-        :param key: The key of the dispenser to interact with.
-        :param proj: The projectile to put in the dispenser.
-        """
-        if key not in self.dispensers:
-            log.debug(f"Dispenser not found, creating one...")
-            self.dispensers[key] = engi.Dispenser()
-
-        dispenser = self.dispensers[key]
-
-        log.debug("Getting running loop...")
-        loop = asyncio.get_running_loop()
-
-        for conversation in self.conversations:
-            log.debug(f"Creating run task for: {conversation!r}")
-            loop.create_task(dispenser.run(conversation, _pda=self), name=f"{repr(conversation)}")
-
-        log.debug("Running a _event loop cycle...")
-        await asyncio.sleep(0)
-
-        log.debug(f"Putting projectile {proj!r} in dispenser {dispenser!r}...")
-        await dispenser.put(proj)
-
-        log.debug("Awaiting another _event loop cycle...")
-        await asyncio.sleep(0)
+        await client.start(bot_token=self.bot_token)
+        try:
+            self._register_events(client)
+            await client.run_until_disconnected()
+        finally:
+            await client.disconnect()
 
 
-# Objects exported by this module
 __all__ = (
-    "TelethonPDA",
+    "TelethonPDAImplementation",
 )
